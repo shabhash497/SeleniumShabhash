7c68f326fc8082b00250307dda50a073d76eb020
shabb560 <shabb560@Techm.postbox.in.th>
2018-07-15 13:41:05 +0530

    commiting

--- ProjectApril2018/src/com/java/basics/StaticMethodsofParentinChild.java
+++
@@ -1,7 +0,0 @@
-package com.java.basics;
-
-public class StaticMethodsofParentinChild {
-     public static void parentStaticMethod(){
-    	 System.out.println("Static method of parent class");
-     }
-}
